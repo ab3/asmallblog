69020dbc992981cfc78d3115f253611ba74ac9ad
Werner Laurensse (abe) <el.lauwer@gmail.com>
2010-02-15 19:50:51 +0100

    added and implemented LatestComment to feeds.py

--- feeds.py
+++ feeds.py
@@ -1,6 +1,6 @@
 from django.contrib.syndication.feeds import Feed
 from django.utils import feedgenerator
-from models import Entry
+from models import Entry, Comment
 from datetime import datetime, time
 
 class LatestEntries(Feed):
@@ -9,10 +9,9 @@ class LatestEntries(Feed):
     title = 'Abeocracy.be'
     link = 'http://abeocracy.be'
     description = 'The latest entries from abeocracy.be'
-    # subtitle = 'The latest entries from abeocracy.be'
     
     def items(self):
-        return Entry.objects.filter(listed=True)[:10] #order_by('-pub_date')[:10]
+        return Entry.objects.filter(listed=True)[:10]
     
     def item_title(self, item):
         return item.title
@@ -26,5 +25,31 @@ class LatestEntries(Feed):
     def item_pubdate(self, item):
         return item.pub_date
         
+    def item_categories(self, item):
+        return item.tags.all()
+
+
+class LatestComment(Feed):
+    feed_type = feedgenerator.Rss201rev2Feed
+
+    title = 'Abeocracy.be'
+    link = 'http://abeocracy.be'
+    description = 'The latest comments from abeocracy.be'
+
+    def items(self):
+        return Comment.objects.filter()[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.text
+
+    def item_link(self, item):
+        return item.get_absolute_url()
+
+    def item_pubdate(self, item):
+        return item.pub_date
+
     def item_categories(self, item):
         return item.tags.all()
